92bd3fc686a615ee5a230c0852c4d9e90dafc40b
yingyin <clevershadow@gmail.com>
2011-09-11 20:47:49 -0400

    Moved definition to the header file so that the template class can be compiled correctly.

--- deque/deque.h
+++ deque/deque.h
@@ -1,9 +1,41 @@
-#include "deque.h"
+#ifndef DEQUE_H_
+#define DEQUE_H_
+
+#include <string>
+#include <iostream>
 #include <sstream>
-#include <stdlib.h>
 
 using namespace std;
 
+template <class T>
+struct Node {
+  T value;
+  Node<T> *next, *prev;
+};
+
+template <class T>
+class Deque {
+  public:
+  Deque();
+  void PushBack(const T& item);
+  void PushFront(const T& item);
+  Node<T>* PopBack();
+  Node<T>* PopFront();
+  bool IsEmpty() { return head_ == NULL; }
+  string ToString();  
+
+  private:
+  Node<T> *head_;
+  Node<T>* MakeNewNode(const T& item, Node<T>* next = 0, Node<T>* prev = 0);
+};
+
+// From the point of view of the compiler, templates are not normal functions
+// or classes. They are compiled on demand, meaning that the code of a template
+// function is not compiled until an instantiation with specific template
+// arguments is required.
+//
+// Because templates are compiled when required, the implementation of a 
+// template class or function must be in the same file as its declaration.
 template <class T>
 Deque<T>::Deque() {
   head_ = NULL;
@@ -20,8 +52,8 @@ void Deque<T>::PushFront(const T& item) {
     new_node = MakeNewNode(item, head_, head_->prev);
     head_->prev->next = new_node;
     head_->prev = new_node;
-    head_ = new_node;
   }
+  head_ = new_node;
 }
 
 template <class T>
@@ -31,6 +63,7 @@ void Deque<T>::PushBack(const T& item) {
     new_node = MakeNewNode(item); 
     new_node->next = new_node;
     new_node->prev = new_node;
+    head_ = new_node;
   } else {
     new_node = MakeNewNode(item, head_, head_->prev);
     head_->prev->next = new_node;
@@ -72,7 +105,7 @@ Node<T>* Deque<T>::PopBack() {
 template <class T>
 string Deque<T>::ToString() {
   ostringstream oss;
-  if (head_ == NULL)
+  if (IsEmpty())
     return oss.str();
   Node<T>* cursor = head_;
   do {
@@ -83,9 +116,11 @@ string Deque<T>::ToString() {
 }
 
 template <class T>
-Node<T>* MakeNewNode(const T& item, Node<T>* next, Node<T>* prev) {
+Node<T>* Deque<T>::MakeNewNode(const T& item, Node<T>* next, Node<T>* prev) {
   Node<T> *node = new Node<T>;
+  node->value = item;
   node->next = next;
   node->prev = prev;
   return node;
 } 
+#endif // DEQUE_H_
