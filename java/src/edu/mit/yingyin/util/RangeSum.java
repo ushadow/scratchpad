55ccbc08ed6fe2a4e63ca6c8e6d4468286afe886
yingyin <clevershadow@gmail.com>
2011-12-16 00:03:49 -0500

    Added update method and comments.

--- java/src/edu/mit/yingyin/util/RangeSum.java
+++ java/src/edu/mit/yingyin/util/RangeSum.java
@@ -3,10 +3,22 @@ package edu.mit.yingyin.util;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <code>RangeSum</code> allows both range sum queries of an array of 
+ * integers and value updates in O(log n) time. 
+ * 
+ * @author yingyin
+ *
+ */
 public class RangeSum {
 
-  public static class Node {
-    public int sum, size;
+  private static class Node {
+    public int sum;
+    
+    /**
+     * Number of elements that the sum represents.
+     */
+    public int size;
     public Node left, right, parent;
     
     Node(int sum, int size) {
@@ -15,9 +27,11 @@ public class RangeSum {
     }
     
     /**
+     * Creates a <code>Node</code> with <code>left</code> and <code>right</code>
+     * children.
      * 
-     * @param left is never null.
-     * @param right can be null.
+     * @param left left child should never be null.
+     * @param right right child can be null.
      */
     Node(Node left, Node right) {
       this.left = left;
@@ -32,46 +46,110 @@ public class RangeSum {
   }
   
   private Node root;
+  private List<Node> elements;
   
+ /**
+  * Sets the <code>RangeSum</code> with the <code>array</code> for queries.
+  * 
+  * @param array array of integers for queries.
+  */
   public void setup(int[] array) {
     List<Node> nodes = new ArrayList<Node>();
     for (int i = 0; i < array.length; i++) {
       nodes.add(new Node(array[i], 1));
     }
+    elements = nodes;
     while (nodes.size() > 1) {
       List<Node> parents = new ArrayList<Node>();
       for (int i = 0; i < nodes.size() / 2; i++) {
-        Node node = new Node(nodes.get(i * 2), nodes.get(i * 2 + 1));
-        nodes.get(i * 2).parent = node;
-        nodes.get(i * 2 + 1).parent = node;
+        Node left = nodes.get(i * 2);
+        Node right = nodes.get(i * 2 + 1);
+        Node node = new Node(left, right );
+        left.parent = node;
+        right.parent = node;
         parents.add(node);
       }
       if (nodes.size() % 2 == 1) {
-        Node node = new Node(nodes.get(nodes.size() - 1), null);
+        Node left = nodes.get(nodes.size() - 1);
+        Node node = new Node(left, null);
         parents.add(node);
-        nodes.get(nodes.size() - 1).parent = node;
+        left.parent = node;
       }
       nodes = parents;
     }
     
     root = nodes.get(0);
+    checkRI();
   }
   
+  /**
+   * Returns the sum of elements i to j inclusive.
+   * 
+   * @param i index of the starting element such that i >= 0 and 
+   *    i < <code>array</code>.length.
+   * @param j index of the ending element such that j >= i and j >=0 and 
+   *    j < <code>array</code>.length.
+   * @return the sum of the elements from indices i to j.
+   */
   public int sum(int i, int j) {
     return sum0(j, root) - sum0(i - 1, root);
   }
   
-  public int sum0(int i, Node node) {
-    if (i <= 0)
+  /**
+   * Updates the value of an element in the <code>array</code>.
+   * 
+   * @param index the index of the element to be updated.
+   * @param value the new value of that element.
+   */
+  public void update(int index, int value) {
+    Node element = elements.get(index);
+    element.sum = value;
+    Node current = element.parent;
+    while (current != null) {
+      current.sum = current.left.sum;
+      if (current.right != null)
+        current.sum += current.right.sum;
+      current = current.parent;
+    }
+    checkRI();
+  }
+  
+  private int sum0(int i, Node node) {
+    if (i < 0)
       return 0;
     
     Node current = node;
-    while (current.size > i) 
+    while (current.size > i + 1) 
       current = current.left;
     
-    if (current.size == i)
+    if (current.size == i + 1)
       return current.sum;
     
     return current.sum + sum0(i - current.size, current.parent.right);
+  }
+  
+  private void checkRI() {
+    assert root.parent == null;
+    assert root != null;
+    checkSumAndSize(root);
+  }
+  
+  private void checkSumAndSize(Node node) {
+    if (node.left == null && node.right == null) { 
+      assert node.size == 1;
+    } else {
+      assert node.left != null;
+      int sum = node.left.sum;
+      int size = node.left.size;
+      if (node.right != null) {
+        sum += node.right.sum;
+        size += node.right.size;
+      }
+      assert node.sum == sum;
+      assert node.size == size;
+      checkSumAndSize(node.left);
+      if (node.right != null)
+        checkSumAndSize(node.right);
+    }
   }
 }
--- java/test/edu/mit/yingyin/util/RangeSumTest.java
+++ java/test/edu/mit/yingyin/util/RangeSumTest.java
@@ -7,10 +7,51 @@ import org.junit.Test;
 public class RangeSumTest {
 
   @Test
-  public void test() {
+  public void testSimple() {
     RangeSum rs = new RangeSum();
     rs.setup(new int[] {1, 2, 3, 4, 5});
     assertEquals(6, rs.sum(0, 2));
+    assertEquals(1, rs.sum(0, 0));
+    assertEquals(5, rs.sum(4, 4));
+    assertEquals(15, rs.sum(0, 4));
+    assertEquals(7, rs.sum(2, 3));
+    assertEquals(12, rs.sum(2, 4));
   }
 
+  @Test
+  public void testOne() {
+    RangeSum rs = new RangeSum();
+    rs.setup(new int[] {0});
+    assertEquals(0, rs.sum(0, 0));
+  }
+  
+  @Test
+  public void testPowerOfTwoElements() {
+    RangeSum rs = new RangeSum();
+    rs.setup(new int[] {1, 1, 3, 1});
+    assertEquals(5, rs.sum(0, 2));
+    assertEquals(1, rs.sum(0, 0));
+    assertEquals(1, rs.sum(3, 3));
+    assertEquals(6, rs.sum(0, 3));
+    assertEquals(4, rs.sum(2, 3));
+    assertEquals(5, rs.sum(1, 3));
+  }
+  
+  @Test
+  public void testUpdate() {
+    RangeSum rs = new RangeSum();
+    rs.setup(new int[] {1, 2, 3, 4, 5});
+    rs.update(0, 6);
+    assertEquals(6, rs.sum(0, 0));
+    assertEquals(20, rs.sum(0, 4));
+    assertEquals(7, rs.sum(2, 3));
+  }
+  
+  @Test 
+  public void tesetUpdateOneElement() {
+    RangeSum rs = new RangeSum();
+    rs.setup(new int[] {0});
+    rs.update(0, 6);
+    assertEquals(6, rs.sum(0, 0));
+  }
 }
