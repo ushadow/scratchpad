e29e608ced263912a9e020c1f01046c0667a97a2
Ying Yin <clevershadow@gmail.com>
2014-04-08 20:33:09 -0400

    Added gradient2.

--- c++/gradient/gradient.cpp
+++ c++/gradient/gradient.cpp
@@ -6,11 +6,12 @@
 
 namespace {
 
-//#define DEBUG 
-
 typedef unsigned char data_t;
 #define DATA_MAX 256
+
+#ifndef NUM_REPETITIONS
 #define NUM_REPETITIONS 1
+#endif
 
 class StopWatch {
 public:
@@ -18,6 +19,7 @@ public:
     tic_ = clock();
   }
 
+  // Returns time elapsed in ms.
   int toc(const char* msg) {
     clock_t t = clock();
     int elapsed = int((t - tic_) * 1000 / CLOCKS_PER_SEC);
@@ -60,10 +62,11 @@ void minmax(const float* d, int n, float* min, float* max) {
   }
 }
 
+// Computes dx and dy in the same loop.
 // Returns the total time taken in ms.
 int gradient(data_t* M, int w, int h) {
   float ry, rx;
-  data_t *My, *My0, *My1, *Mx0, *Mx1;
+  data_t *My, *My0, *My1, *Mx0, *Mx1; // indices
   float *dx, *dy, *dxp, *dyp;
   StopWatch sw;
 
@@ -108,12 +111,68 @@ int gradient(data_t* M, int w, int h) {
   return sw.toc("Total");
 }
 
+// Computes dx and dy in separate loops.
+// This seems to be faster.
+int gradient2(data_t* M, int w, int h) {
+  float ry, rx;
+  data_t *My, *My0, *My1, *Mx0, *Mx1; // indices
+  float *dx, *dy, *dxp, *dyp;
+  float dx_max, dy_max, dx_min, dy_min;
+  StopWatch sw;
+
+  sw.tic();
+  
+  int n = w * h;
+  dx = dxp = new float[n];
+  dy = dyp = new float[n];
+
+  My = M; // My is the center index.
+  for (int y = 0; y < h; y++) {
+    for (int x = 0; x < w; x++) {
+      if (x == 0)     { Mx0 = My;     Mx1 = My + 1; rx = 1; } 
+      if (x == 1)     { Mx0 = My - 1; Mx1 = My + 1; rx = .5; }
+      if (x == w - 1) {               Mx1 = My;     rx = 1; }
+      *(dxp++) = (*Mx1 - *Mx0) * rx;
+      Mx0++; Mx1++; My++;
+    }
+  }
+  minmax(dx, n, &dx_min, &dx_max);
+
+  My = M;
+  for (int y = 0; y < h; y++) {
+    if (y == 0)     { My0 = My;     My1 = My + w; ry = 1; }
+    if (y == 1)     { My0 = My - w; My1 = My + w; ry = .5; }
+    if (y == h - 1) {               My1 = My;     ry = 1; }
+    for (int x = 0; x < w; x++) {
+      *(dyp++) = (*My1 - *My0) * ry;
+      My0++; My1++; My++;
+    }
+  }
+  
+  minmax(dy, n, &dy_min, &dy_max);
+
+  printf("min dx = %.1f\n", dx_min);
+  printf("max dx = %.1f\n", dx_max);
+  printf("min dy = %.1f\n", dy_min);
+  printf("max dy = %.1f\n", dy_max);
+
+#ifdef DEBUG
+  print_matrix(M, w, h);
+  print_matrix(dx, w, h);
+  print_matrix(dy, w, h);
+#endif
+
+  delete [] dx;
+  delete [] dy;
+  return sw.toc("Total");
+}
+
 // w * h should be <= INT_MAX
 int compute(int w, int h) {
   int n = w * h;
   data_t* M = new data_t[n];
   init_random(M, n);
-  int elapsed = gradient(M, w, h);
+  int elapsed = gradient2(M, w, h);
   delete [] M;
   return elapsed;
 }
